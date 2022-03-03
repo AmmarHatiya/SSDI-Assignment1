11bc94c34f57b1b8040861bea312a5bedecdd02d
ammarh <ammar.hatiya@gmail.com>
2022-03-03 18:52:23 -0500

    Added GamingPC builder class

---
+++ app/src/main/java/SSDI/Assignment1/GamingPC.java
@@ -0,0 +1,36 @@
+package SSDI.Assignment1;
+
+public class GamingPC {
+        /* Inherited Attributes */
+        private int StorageVal;
+        private String CPUName;
+        private int ScreenResolution;
+        private int ScreenSize;
+        private boolean Portable;
+        private String FanName;
+    
+        /* New Attributes */
+        private boolean RGB;
+        private int MonitorNum;
+
+        /* Constructor */
+        public GamingPC(int StorageVal,
+         String CPUName,
+         int ScreenResolution,
+         int ScreenSize,
+         boolean Portable,
+         String FanName, boolean RGB, int MonitorNum){
+
+            this.StorageVal = StorageVal;
+            this.CPUName= CPUName;
+            this.ScreenResolution =ScreenResolution ;
+            this.ScreenSize =ScreenSize ;
+            this.Portable =Portable ;
+            this.FanName = FanName;
+            this.RGB =RGB ;
+            this.MonitorNum = MonitorNum;
+
+         }
+
+    
+}
--- app/src/main/java/SSDI/Assignment1/GamingPCBuilder.java
+++ app/src/main/java/SSDI/Assignment1/GamingPCBuilder.java
@@ -1,5 +1,4 @@
 package SSDI.Assignment1;
-import SSDI.Assignment1.Builder;
 
 public class GamingPCBuilder implements Builder{
     /* Inherited Attributes */
@@ -13,5 +12,48 @@ public class GamingPCBuilder implements Builder{
     /* New Attributes */
     private boolean RGB;
     private int MonitorNum;
-        
+    public GamingPCBuilder(){
+    
+    } 
+    @Override
+    public GamingPCBuilder setFanName(String FanName){
+        this.FanName = FanName;
+        return this;
+    }
+
+    @Override
+    public GamingPCBuilder setCPUName(String CPUName){
+        this.CPUName = CPUName;
+        return this;
+    }
+    @Override
+    public GamingPCBuilder setStorageVal(int StorageVal){
+        this.StorageVal = StorageVal;
+        return this;
+    }
+    @Override
+    public GamingPCBuilder setScreenSize(int ScreenSize){
+        this.ScreenSize = ScreenSize;
+        return this;
+    }
+    @Override
+    public GamingPCBuilder setScreenResolution(int ScreenResolution){
+        this.ScreenResolution = ScreenResolution;
+        return this;
+    }
+    @Override
+    public GamingPCBuilder setPortable(boolean Portable){
+        this.Portable = Portable;
+        return this;
+    }
+    
+    public GamingPC createGamingPC(){
+        return new GamingPC(StorageVal, CPUName, ScreenResolution, ScreenSize, Portable, FanName, RGB, MonitorNum);
+    }
+
+
+
+
+
+  
 }
